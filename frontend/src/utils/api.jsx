a8fb1e60348726303a9475d7906cd197dcab55a7
godyracks <godfreymatagaro@gmail.com>
2025-05-02 07:06:45 +0300

    updated feedback form

--- frontend/src/utils/api.jsx
+++ frontend/src/utils/api.jsx
@@ -1,8 +1,19 @@
 // frontend/src/utils/api.js
 import axios from 'axios';
 
+// Determine baseURL based on environment
+const getBaseUrl = () => {
+  const isProduction = import.meta.env.MODE === 'production';
+  const devUrl = import.meta.env.VITE_API_DEV_BACKEND_URL; // http://localhost:3000
+  const prodUrl = import.meta.env.VITE_API_PROD_BACKEND_URL; // https://eduability.onrender.com/
+
+  // Use production or development URL, ensuring /api is appended
+  const baseUrl = isProduction ? `${prodUrl}/api` : `${devUrl}/api`;
+  return baseUrl;
+};
+
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api', // Adjust baseURL based on your backend
+  baseURL: getBaseUrl(), // Dynamically set baseURL
   headers: {
     'Content-Type': 'application/json',
   },

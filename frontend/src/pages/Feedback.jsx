c80339ed196cc301fcb1341bdae187343c9f2b13
godyracks <godfreymatagaro@gmail.com>
2025-05-02 08:32:10 +0300

    redesignedfeedback form

--- frontend/src/pages/Feedback.jsx
+++ frontend/src/pages/Feedback.jsx
@@ -1,10 +1,12 @@
 // src/pages/Feedback.jsx
 import Footer from '../components/layout/Footer';
 import FeedbackForm from '../components/layout/FeedbackForm';
+import Reviews from '../components/layout/Reviews';
 const Feedback = () => {
     return (
         <div>
         <FeedbackForm />
+        <Reviews />
           <Footer />
         </div>
       );

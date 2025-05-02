6649b2ac6bc0be05fe6f75513fe1a60fd365f999
godyracks <godfreymatagaro@gmail.com>
2025-05-02 10:12:09 +0300

    made this a frontend repo

--- backend/routes/search.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const searchController = require('../controllers/search.controller');
-
-router.get('/', searchController.search);
-
-module.exports = router;
